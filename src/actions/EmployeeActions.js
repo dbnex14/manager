81b2707810b8334ec3b15bcbdc80fc3cbfaa10d9
dino buljubasic <dbnex14@gmail.com>
2018-02-19 20:23:18 -0800

    Added logic to save new employee to firebase, navigate back to EmployeeListForm and also clear EmployeeCreate form once employee is persisted to firebase.

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,13 +1,30 @@
 //
 // action creator for employeeCreate LoginForm
 //
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop();
+      });
   };
 };
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   onButtonPress() {
       const { name, phone, shift } = this.props;
 
-      this.props.employeeCreate({ name, phone, shift });
+      this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
   }
-  
+
   render() {
     return (
       <Card>
@@ -74,4 +74,6 @@ const mapStateToProps = (state) => {
 };
 
 //export default EmployeeCreate;
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+  employeeUpdate, employeeCreate
+})(EmployeeCreate);
